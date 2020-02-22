1863a52dc81bb6df1e9980c83d12871a26a77188
Lorwik <lorwik@gmail.com>
2020-02-23 00:48:21 +0100

    Añadido pagina personalizada de error 404

---
+++ 404.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/estilos-login.css">
+		<link rel="shortcut icon" href="nhicon.ico"/>
+		<?php include("includes/config.php")?>
+		<title><?php echo $titulo; ?> - Error 404</title>
+	</head>
+	<body>
+		<a href="index.php"><img src="img/logo.png" class="logo" width="150"></a>
+
+		<center>
+		<h1>¡Vaya! Esa página no se puede encontrar.</h1>
+		<h2>ERROR 404</h2>
+	    </center>
+	</body>
+</html>

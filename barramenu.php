7ef6775ee2338db3fab9b5c3359ebc5541e57ac9
Lorwik <lorwik@gmail.com>
2020-02-22 23:05:46 +0100

    Eliminado 'Musica' y añadido enlace a FilmAffinity y MyAnimeList

--- barramenu.php
+++ barramenu.php
@@ -1,5 +1,6 @@
 <ul class="menu-superior">
 	<li><a href="index.php">Inicio</a></li>
-	<li><a href="">Música</a></li>
 	<li><a href="youtube.php">YouTube</a></li>
+	<li><a href="https://www.filmaffinity.com/es/main.html" target="_blank">FilmAffinity</a></li>
+	<li><a href="https://myanimelist.net/" target="_blank">MyAnimeList</a></li>
 </ul>

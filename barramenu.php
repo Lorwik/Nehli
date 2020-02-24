371a13dd780ffa82e45743b44fdc9c9a9a56d3ac
Lorwik <lorwik@gmail.com>
2020-02-24 01:53:17 +0100

    Arreglado pequeño problema del menu

---
+++ barramenu.php
@@ -0,0 +1,9 @@
+<ul class="menu-superior">
+	<li><a href="index.php">Inicio</a></li>
+	<li><a href="">Música</a></li>
+	<li><a href="youtube.php">YouTube</a></li>
+</ul>
+
+
+<h1>Bienvenido <?php echo $user->getNombre();  ?></h1>
+<li class="cerrar-sesion"><a href="includes/logout.php">Cerrar sesión</a></li>

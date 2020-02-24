1a4a3f5b00494dd8a7a4841480b66dce25b0185c
Lorwik <lorwik@gmail.com>
2020-02-24 02:15:53 +0100

    Mejoras en el menu

--- barramenu.php
+++ barramenu.php
@@ -4,6 +4,8 @@
 	<li><a href="youtube.php">YouTube</a></li>
 </ul>
 
-
-<h1>Bienvenido <?php echo $user->getNombre();  ?></h1>
-<li class="cerrar-sesion"><a href="includes/logout.php">Cerrar sesión</a></li>
+<ul id="menu-superiorright" class="menu-superior">
+	<li>Bienvenido <?php echo $user->getNombre(); ?></li>
+	<li><a href="includes/logout.php">Cerrar sesión</a></li>
+	<li><img class="avatar" src="img/avatar.png"></li>
+</ul>

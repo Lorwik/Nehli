66ce6a38bba52e2c178719a7369278418ce350e2
Lorwik <lorwik@gmail.com>
2020-02-24 03:04:39 +0100

    Arreglado barramenu

--- barramenu.php
+++ barramenu.php
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-<ul class="menu-superior">
-	<li><a href="index.php">Inicio</a></li>
-	<li><a href="">Música</a></li>
-	<li><a href="youtube.php">YouTube</a></li>
-</ul>
-
-
-<h1>Bienvenido <?php echo $user->getNombre();  ?></h1>
-<li class="cerrar-sesion"><a href="includes/logout.php">Cerrar sesión</a></li>
-=======
 <ul class="menu-superior">
 	<li><a href="index.php">Inicio</a></li>
 	<li><a href="">Música</a></li>
@@ -20,4 +9,3 @@
 	<li><a href="includes/logout.php">Cerrar sesión</a></li>
 	<li><img class="avatar" src="img/avatar.png"></li>
 </ul>
->>>>>>> registro

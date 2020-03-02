5fdbfff2bc9044f7789ab08ea97150698387d6a3
Lorwik <manueljsandalio@gmail.com>
2020-03-02 18:31:15 +0100

    Creado panel de administracion

---
+++ adminpanel.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<?php
+			$pagina = " Panel de Administración";
+			include("header.php");
+		?>
+		<script src="js/funciones.js"></script>
+	</head>
+
+	<body>
+		<center>
+			<div class="listado">
+				<h1>Panel de Administración</h1>
+
+				<div class="contentseparador">
+					
+				</div>
+			</div>
+		</center>
+	</body>
+</html>
